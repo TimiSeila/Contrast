496a97027a08d32ace11923ae4d45017eff1cf59
NoLifeDark <s2101049@edu.bc.fi>
2023-10-12 12:54:59 +0300

    Tein paskaa ja rikoin koko homman
    Kaikki UI lisätty. Koottu koko homma

---
+++ Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void SelectMap()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void SelectMap2()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void SelectMap3()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
